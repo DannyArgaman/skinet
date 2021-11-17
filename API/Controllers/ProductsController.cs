8aafeb5c215fab2af222271c69c708d1d9713d9c
Danny Argaman <ar_Dan@hotmail.com>
2021-11-17 14:38:27 +0200

    End of Section 6

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -11,6 +11,7 @@ using Core.Specifications;
 using API.Dtos;
 using AutoMapper;
 using API.Errors;
+using API.Helpers;
 
 namespace API.Controllers
 {
@@ -33,13 +34,18 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productparams)
         {
-            var spec= new ProductsWithTypesAndBrandsSpecification();
+            var spec= new ProductsWithTypesAndBrandsSpecification(productparams);
+            var countSpec = new ProductWithFiltersForCountSpecificication(productparams);
+            var totalItems= await _productsRepo.CountAsync(countSpec);
             var products= await _productsRepo.ListAsync(spec);
+            var data = _mapper
+                .Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products);
+
             
-            return Ok(_mapper
-                .Map<IReadOnlyList<Product>,IReadOnlyList<ProductToReturnDto>>(products));
+            
+            return Ok(new Pagination<ProductToReturnDto>(productparams.PageIndex, productparams.PageSize, totalItems, data));
         }
 
         [HttpGet("{id}")]
--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -31,17 +31,24 @@ namespace Infrastructure.Data
         
         public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
         {
-            return await ApplySpecificatuion(spec).FirstOrDefaultAsync();
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
         }
 
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
         {
-            return await ApplySpecificatuion(spec).ToListAsync();
+            return await ApplySpecification(spec).ToListAsync();
         }
 
-        private IQueryable<T> ApplySpecificatuion(ISpecification<T> spec)
+          public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
         {
             return SpecifationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
+
+      
     }
 }
